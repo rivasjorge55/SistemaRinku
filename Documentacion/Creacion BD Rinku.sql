513a622c0deb3059ee8c289af067058d10c7f66e
rivasjorge55 <rivasjorge55@gmail.com>
2020-01-28 19:30:32 -0700

    Create Creacion BD Rinku.sql

---
+++ Documentacion/Creacion BD Rinku.sql
@@ -0,0 +1,37 @@
+--Creacion tabla roles
+create table roles
+(
+	id int not null primary key identity,
+	nombre varchar(30) not null,
+	activo bit not null
+);
+
+--creacion tabla tipos
+create table tipos
+(
+	id int not null primary key identity,
+	nombre varchar(15),
+	activo bit not null
+);
+
+--Creacion Tabla trabajadores
+Create table trabajadores
+(
+	id int not null primary key identity,
+	nombre varchar(100) not null,
+	idrol int not null foreign key references roles(id),
+	idtipo int not null foreign key references tipos(id),
+	activo bit not null
+);
+
+--creacion tabla movimientos
+
+create table movimientos 
+(
+	id int not null primary key identity,
+	idtrabajador int not null foreign key references trabajadores(id),
+	fecha date not null,
+	idrolpropio int not null foreign key references roles(id), --CAMPO informativo, en prevision de que los empleados puedan cambiar de rol en un futuro
+	idrolejecutado int not null foreign key references roles(id), --Este es el que se empleara para hacer el calculo de pago
+	entregas int not null default 0
+)
