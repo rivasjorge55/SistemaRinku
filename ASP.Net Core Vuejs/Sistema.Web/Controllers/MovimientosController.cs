7f331ce73d23190772a7959d0bc3b0e50b8336cb
rivasjorge55 <rivasjorge55@gmail.com>
2020-02-01 11:31:10 -0700

    Creacion controller movimientos

--- ASP.Net Core Vuejs/Sistema.Web/Controllers/MovimientosController.cs
+++ ASP.Net Core Vuejs/Sistema.Web/Controllers/MovimientosController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sistema.Datos;
 using Sistema.Entidades.Registros;
+using Sistema.Web.Models.Movimientos;
 
 namespace Sistema.Web.Controllers
 {
@@ -21,22 +22,33 @@ namespace Sistema.Web.Controllers
             _context = context;
         }
 
-        // GET: api/Movimientos
-        [HttpGet]
-        public IEnumerable<Movimiento> GetMovimientos()
+        // GET: api/Movimientos/Listar
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<MovimientosViewModel>> Listar()
         {
-            return _context.Movimientos;
+            var trabajador = await _context.Movimientos.ToListAsync();
+            return trabajador.Select(m => new MovimientosViewModel
+            {
+                id = m.id,
+                idtrabajador=m.idtrabajador,
+                idrol=m.idrol,
+                fecha=m.fecha,
+                entregas=m.entregas 
+            });
         }
 
-        // GET: api/Movimientos/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetMovimiento([FromRoute] int id)
+        // GET: api/Movimientos/vwmovimientos
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<vw_movimientos>> vwmovimientos()
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            var movimiento = await _context.vw_movimientos.ToListAsync();
+            return movimiento;
+        }
 
+        // GET: api/Movimientos/Mostrar/3
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> Mostrar([FromRoute] int id)
+        {
             var movimiento = await _context.Movimientos.FindAsync(id);
 
             if (movimiento == null)
@@ -44,62 +56,89 @@ namespace Sistema.Web.Controllers
                 return NotFound();
             }
 
-            return Ok(movimiento);
+            return Ok(new MovimientosViewModel
+            {
+                id = movimiento.id,
+                idtrabajador = movimiento.idtrabajador,
+                idrol = movimiento.idrol,
+                fecha = movimiento.fecha,
+                entregas = movimiento.entregas
+            });
         }
 
-        // PUT: api/Movimientos/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMovimiento([FromRoute] int id, [FromBody] Movimiento movimiento)
+        // PUT: api/Movimientos/Actualizar
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Actualizar([FromBody] ActualizarViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            if (id != movimiento.id)
+            if (model.id <= 0)
             {
                 return BadRequest();
             }
 
-            _context.Entry(movimiento).State = EntityState.Modified;
+            var movimiento = await _context.Movimientos.FirstOrDefaultAsync(m => m.id == model.id);
 
+            if (movimiento == null)
+            {
+                return NotFound();
+            }
+
+            movimiento.idtrabajador = model.idtrabajador;
+            movimiento.idrol = model.idrol;
+            movimiento.fecha = model.fecha;
+            movimiento.entregas = model.entregas;
+            
             try
             {
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!MovimientoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                //Guardar Excepcion
+                return BadRequest();
             }
 
-            return NoContent();
+            return Ok();
         }
 
-        // POST: api/Movimientos
-        [HttpPost]
-        public async Task<IActionResult> PostMovimiento([FromBody] Movimiento movimiento)
+        // POST: api/Movimientos/Crear
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Crear([FromBody] CrearViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            Movimiento movimiento = new Movimiento
+            {
+                idtrabajador = model.idtrabajador,
+                idrol = model.idrol,
+                fecha = model.fecha,
+                entregas = model.entregas
+            };
+
             _context.Movimientos.Add(movimiento);
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
 
-            return CreatedAtAction("GetMovimiento", new { id = movimiento.id }, movimiento);
+
+            return Ok();
         }
-
-        // DELETE: api/Movimientos/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMovimiento([FromRoute] int id)
+        ///
+        // DELETE: api/Movimientos/Eliminar/3
+        [HttpDelete("[action]/{id}")]
+        public async Task<IActionResult> Eliminar([FromRoute] int id)
         {
             if (!ModelState.IsValid)
             {
@@ -113,11 +152,17 @@ namespace Sistema.Web.Controllers
             }
 
             _context.Movimientos.Remove(movimiento);
-            await _context.SaveChangesAsync();
-
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                return BadRequest();
+            }
             return Ok(movimiento);
         }
-
+        
         private bool MovimientoExists(int id)
         {
             return _context.Movimientos.Any(e => e.id == id);
